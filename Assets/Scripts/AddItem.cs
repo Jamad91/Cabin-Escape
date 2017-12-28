4f86600e2f2a1ff58a5159f58e19bd846267d14d
Jimmy <jdicolandrea@gmail.com>
2017-12-27 19:48:27 -0500

    add sprites to inventory

--- Assets/Scripts/AddItem.cs
+++ Assets/Scripts/AddItem.cs
@@ -1,26 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddItem : MonoBehaviour {
 
-	public Inventory inventory;
+	private Inventory inventory;
 
 	void OnMouseDown ()
 	{
-//		Debug.Log ("clicking");
-//		Debug.Log (gameObject.name);
-//		Debug.Log (gameObject.tag);
-//		inventory = gameObject.GetComponent<Inventory> ();
-//		levelManager = GameObject.FindObjectOfType<LevelManager>();
-//		inventory = gameObject.GetComponent<Inventory>();
-//		GameObject.Find("nameOfObjectYourScriptIsOn").GetComponent<move>().speed
 		inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
-		if (inventory == null) {
-			Debug.Log("null time");
-		}
-//		inventory.AddItem();
-		Debug.Log("inventory:" + inventory.items.Length);
+//			.GetComponent<Image>().sprite
+//		Debug.Log(gameObject.GetComponent<SpriteRenderer>().sprite.GetType());
+		inventory.AddItem(gameObject);
 
 	}
 }
+
+
+//public string NameOfItemToEquip;
+//	public string whereToEquipItem;
+//	public int quality = 0;
+//	public string NameOfSlotImage;
+//	public GameObject backpack; 
+//	// Use this for initialization
+//	void Start () {
+//
+//		SetNameColor (quality);
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//
+//
+//	void OnTriggerEnter(Collider other){
+//		if (other.gameObject.tag == "Player") {
+////			if (whereToEquipItem == "rightHand") {
+////				GameObject item = Instantiate (Resources.Load (NameOfItemToEquip, typeof(GameObject))) as GameObject;
+////				other.gameObject.GetComponent<EquipItemScript> ().EquipRightHandWith (item);
+////			} else if (whereToEquipItem == "leftHand") {
+////				GameObject item = Instantiate (Resources.Load (NameOfItemToEquip, typeof(GameObject))) as GameObject;
+////				other.gameObject.GetComponent<EquipItemScript> ().EquipShield (item);
+////			} else if (whereToEquipItem == "head") {
+////				GameObject item = Instantiate (Resources.Load (NameOfItemToEquip, typeof(GameObject))) as GameObject;
+////				other.gameObject.GetComponent<EquipItemScript> ().EquipHeadWith (item);
+////			} else if (whereToEquipItem == "buckle") {
+////				GameObject item = Instantiate (Resources.Load (NameOfItemToEquip, typeof(GameObject))) as GameObject;
+////				other.gameObject.GetComponent<EquipItemScript> ().EquipBuckleWith (item);
+////			} else if (whereToEquipItem == "shoulders") {
+////				string leftShoulderName = NameOfItemToEquip + "-left";
+////				string rightShoulderName = NameOfItemToEquip + "-right";
+////				GameObject itemLeft = Instantiate (Resources.Load (leftShoulderName, typeof(GameObject))) as GameObject;
+////				GameObject itemRight = Instantiate (Resources.Load (rightShoulderName, typeof(GameObject))) as GameObject;
+////				other.gameObject.GetComponent<EquipItemScript> ().EquipLeftShoulderWith (itemLeft);
+////				other.gameObject.GetComponent<EquipItemScript> ().EquipRightShoulderWith (itemRight);
+////			}
+//
+//			//todo: add item to inventory
+//			GameObject imageObject = Instantiate(Resources.Load(NameOfSlotImage)) as GameObject;
+//				
+//			GameObject player = GameObject.FindGameObjectWithTag("Player");
+////			GameObject backpack = player.GetComponent<EquipItemScript>().backpack;
+//			backpack = player.GetComponent<EquipItemScript>().backpack;
+//
+////			GameObject backpack = GameObject.FindGameObjectWithTag("Backpack");
+//
+//			backpack.GetComponent<BackpackManager>().AddItem(imageObject);
+//			Destroy (this.gameObject);
+//
+//		}
+//	}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,33 +5,33 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
 
-	public Image[] itemImages = new Image[numItemSlots];
-	public Item[] items = new Item[numItemSlots];
-
+	public GameObject[] items = new GameObject[numItemSlots];
 	public const int numItemSlots = 6;
+	private Image itemImage;
 
-	public void AddItem ()
+	public void AddItem (GameObject itemToAdd)
 	{
-//		for (int i = 0; i < items.Length; i++) {
-//			if (items [i] == null) {
-//				items[i] = itemToAdd;
-//				itemImages[i].sprite = itemToAdd.sprite;
-//				itemImages[i].enabled = true;
-				Debug.Log("items: " + items);
-//				return;
-//			}
-//		}	
-	}
-
-	public void RemoveItem (Item itemToRemove) {
+		
 		for (int i = 0; i < items.Length; i++) {
-			if (items [i] == itemToRemove) {
-				items[i] = null;
-				itemImages[i].sprite = null;
-				itemImages[i].enabled = false;
+//			
+			if (items[i].transform.GetChild(0).GetComponent<Image>().sprite == null) {
+				itemImage = items[i].transform.GetChild(0).GetComponent<Image>();
+				itemImage.sprite = itemToAdd.GetComponent<SpriteRenderer>().sprite;
+				Destroy(itemToAdd);
 				return;
 			}
 		}	
 	}
+
+//	public void RemoveItem (Item itemToRemove) {
+//		for (int i = 0; i < items.Length; i++) {
+//			if (items [i] == itemToRemove) {
+//				items[i] = null;
+//				itemImages[i].sprite = null;
+//				itemImages[i].enabled = false;
+//				return;
+//			}
+//		}	
+//	}
 
 }
